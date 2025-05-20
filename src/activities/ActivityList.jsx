a615dadf00b9c18bce3e41b091a2e7debd10cf99
Rachel Davis <rachelddavis@aol.com>
2025-05-20 15:31:24 -0600

    Unit3FitnessTrackr Updates

---
+++ src/activities/ActivityList.jsx
@@ -0,0 +1,40 @@
+import useMutation from "../api/useMutation";
+import useQuery from "../api/useQuery";
+import { useAuth } from "../auth/AuthContext";
+
+export default function ActivityList() {
+  const {
+    data: activities,
+    loading,
+    error,
+  } = useQuery("/activities", "allActivities");
+  console.log(activities);
+  if (loading || !activities) return <p>Loading...</p>;
+  if (error) return <p>Sorry! {error}</p>;
+  return (
+    <ul>
+      {activities.map((activity) => (
+        <ActivityListItem activity={activity} key={activity.id} />
+      ))}
+    </ul>
+  );
+}
+function ActivityListItem({ activity }) {
+  const { token } = useAuth();
+  const {
+    mutate: deleteActivity,
+    loading,
+    error,
+  } = useMutation("DELETE", "/activities/" + activity.id, ["allActivities"]);
+  console.log(token);
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && (
+        <button onClick={() => deleteActivity()}>
+          {loading ? "Deleting" : error ? error : "Delete"}
+        </button>
+      )}
+    </li>
+  );
+}
